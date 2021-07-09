513e4c9933956d56d7904178610152a21cda9abf
cass1618 <cass16180@gmail.com>
2021-07-09 09:45:03 -0700

    :test_tube: Add test for add new keg, good fail

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,7 +1,48 @@
 import KegListReducer from "../../reducers/keg-list-reducer";
 
-describe('KegListReducer', () => {
-    test('Should return default state if there is no action type passed into the reducer', () => {
+describe("KegListReducer", () => {
+
+    let action;
+    const kegData = {
+        brewery: "Modern Times",
+        name: "Black House Coffee Stout",
+        location: "San Diego, CA",
+        description: "Oatmeal coffee stout positively redolent with coffee aroma and flavor.",
+        abv: "5.80%", 
+        price: "$8",
+        volume: 15.5,
+        id: 1
+    }
+    
+    test("Should return default state when no action is passed to reducer", () => {
         expect(KegListReducer({}, { type: null })).toEqual({});
+    });
+
+    test("Should add new keg to kegArray", () => {
+        const {brewery, name, location, description, abv, price, volume, id} = kegData;
+        action = {
+            type: "ADD_KEG",
+            brewery: "Modern Times",
+            name: "Black House Coffee Stout",
+            location: "San Diego, CA",
+            description: "Oatmeal coffee stout positively redolent with coffee aroma and flavor.",
+            abv: "5.80%", 
+            price: "$8",
+            volume: 15.5,
+            id: 1
+        };
+
+        expect(ticketListReducer({}, action)).toEqual({
+            [id] : {
+                brewery: brewery, 
+                name: name,
+                location: location,
+                description: description,
+                abv: abv, 
+                price: price,
+                volume: volume,
+                id: id 
+            }
+        });
     });
 });
